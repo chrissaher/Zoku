67bac390c95cb916298e97fbc38a4cdad9d19120
chrissaher <chrissaher@gmail.com>
2024-07-16 12:33:55 +0900

    Update poetry installation in action file

--- .github/scripts/build_docs.sh
+++ .github/scripts/build_docs.sh
@@ -10,6 +10,7 @@ set -o nounset
 set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
 
 # Create ~/.kaggle directory if it doesn't exist
 mkdir -p ~/.kaggle
